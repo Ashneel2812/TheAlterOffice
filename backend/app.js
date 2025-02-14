2ecd402a64d250889206fc12d2b79e010197e73d
Ashneel <iashneel@gmail.com>
2025-02-14 17:27:41 +0530

    options addition

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,13 @@ const corsOptions = {
 app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://alteroffice-frontend.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  return res.sendStatus(200);
+});
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
